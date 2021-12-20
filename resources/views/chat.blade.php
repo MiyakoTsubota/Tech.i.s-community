9ad960bfe3952f55a60827e75360386b662fba0f
YuTKD <85022390+YuTKD@users.noreply.github.com>
2021-12-20 17:52:41 +0900

    チャット画面制作途中

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -12,19 +12,54 @@
       <th>Chat</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td><span style="color:gray">ユーザーネーム</span><br><p>テキスト</P></td>
-    </tr>
-    <td>チャットチャット</td>
-  </tbody>
 </table>
+
+<!-- つぶやき一覧エリア -->
+<div class="tweet-list">
+<div class="tweet">
+  <div class="user">
+      <a href="profile.php?user_id=1">
+          <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
+      </a>
+  </div>
+  <div class="content">
+      <div class="name">
+          <a href="profile.php?user_id=1">
+              <span class="nickname">太郎</span>
+              <span class="user-name">@taro ・23日前</span>
+          </a>
+      </div>
+      <p>今プログラミングをしています。</p>
+  </div>
+</div>
+<div class="tweet">
+    <div class="user">
+        <a href="profile.php?user_id=2">
+            <img src="../Views/img/icon-default-user.svg" alt="">
+        </a>
+    </div>
+    <div class="content">
+        <div class="name">
+            <a href="profile.php?user_id=2">
+                <span class="nickname">次郎</span>
+                <span class="user-name">@jiro ・24日前</span>
+            </a>
+        </div>
+        <p>コワーキングスペースをオープンしました</p>
+        <img src="../Views/img_uploaded/tweet/sample-post.jpg" alt="" class="post-image">
+    </div>
+</div>
+
+
+
 
 <footer style="position: fixed;bottom: 0; width: 90%;background-color: white;">
   <form action="" method="POST">
-    <div class="form-group">
+    <textarea name="message" placeholder="メッセージ" maxlength="300"></textarea>
+    <div class="file">
+      <input type="file" name="image" class="">
     </div>
-    <button type="submit" class="btn btn-outline-dark my-lg-4 my-4">チャットを投稿</button>
+    <button type="submit" class="btn btn-block btn-info">チャットを投稿</button>
   </form>
 </footer>
 
