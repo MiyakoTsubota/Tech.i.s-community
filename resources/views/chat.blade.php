aaea622389f19297c9b6d3dd76a41ac592dca88e
YuTKD <85022390+YuTKD@users.noreply.github.com>
2021-12-21 21:13:00 +0900

    チャット画面制作中

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -3,9 +3,17 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/chat.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
 @stop
 
-チャット画面
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
 <table class="table mb-5">
   <thead class="thead-dark">
     <tr>
@@ -14,55 +22,74 @@
   </thead>
 </table>
 
-<!-- つぶやき一覧エリア -->
-<div class="tweet-list">
-<div class="tweet">
-  <div class="user">
-      <a href="profile.php?user_id=1">
-          <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
-      </a>
-  </div>
-  <div class="content">
-      <div class="name">
-          <a href="profile.php?user_id=1">
-              <span class="nickname">太郎</span>
-              <span class="user-name">@taro ・23日前</span>
-          </a>
-      </div>
-      <p>今プログラミングをしています。</p>
-  </div>
-</div>
-<div class="tweet">
+<!-- チャット一覧エリア -->
+
+<div class="chat-list">
+  <div class="tweet">
     <div class="user">
-        <a href="profile.php?user_id=2">
-            <img src="../Views/img/icon-default-user.svg" alt="">
+        <a href="profile.php?user_id=1">
+            <img src="" alt="">
         </a>
     </div>
+    <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333;">
     <div class="content">
         <div class="name">
-            <a href="profile.php?user_id=2">
-                <span class="nickname">次郎</span>
-                <span class="user-name">@jiro ・24日前</span>
+            <a href="profile.php?user_id=1">
+                <span class="nickname">太郎</span>
+                <span class="user-name">@taro ・23日前</span>
             </a>
         </div>
-        <p>コワーキングスペースをオープンしました</p>
-        <img src="../Views/img_uploaded/tweet/sample-post.jpg" alt="" class="post-image">
+        <p>今プログラミングをしています</p>
     </div>
+  </div>
+  <div class="tweet">
+      <div class="user">
+          <a href="">
+              <img src="" alt="">
+          </a>
+      </div>
+      </div>
+
+      <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #E9E9E9; background-color;">
+      <div class="content">
+          <div class="name">
+              <a href="profile.php?user_id=2">
+                  <span class="nickname">次郎</span>
+                  <span class="user-name">@jiro ・24日前</span>
+              </a>
+          </div>
+          <p>コワーキングスペースをオープンしました</p>
+          <img src="" alt="" class="post-image">
+      </div>
+  </div>
+  <div class="tweet">
+      <div class="user">
+          <a href="profile.php?user_id=2">
+              <img src="" alt="">
+          </a>
+      </div>
+      </div>
+
+<div class="form-group">
+    <form action="/chat" method="POST">
+    <input class="form-control" type="text" rows="3" placeholder="メッセージ" maxlength="300" id="text">
+    <div class="input-group">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="file">
+                                <label class="custom-file-label" for="exampleInputFile">Choose image</label>
+                            </div>
+                            <div class="input-group-append">
+                                <span class="input-group-text">Upload</span>
+                            </div>
+                        </div>
+      <button type="submit" class="btn btn-block btn-info">チャットを投稿</button>
+    </form>
 </div>
 
 
 
 
-<footer style="position: fixed;bottom: 0; width: 90%;background-color: white;">
-  <form action="" method="POST">
-    <textarea name="message" placeholder="メッセージ" maxlength="300"></textarea>
-    <div class="file">
-      <input type="file" name="image" class="">
-    </div>
-    <button type="submit" class="btn btn-block btn-info">チャットを投稿</button>
-  </form>
-</footer>
 
 
-
+</html>
 @endsection
